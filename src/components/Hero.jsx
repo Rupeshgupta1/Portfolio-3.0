ae808fd792e0b8a69fe33c1536a942a011082da9
Rupeshgupta1 <rupesh272002@gmail.com>
2026-06-06 21:25:14 +0530

    Fix scroll indicator: z-20 above 3D model, centered bottom-10

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -149,7 +149,7 @@ const Hero = () => {
       </div>
 
       {/* Scroll Indicator */}
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center z-10">
+      <div className="absolute bottom-10 w-full flex justify-center items-center z-20">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div

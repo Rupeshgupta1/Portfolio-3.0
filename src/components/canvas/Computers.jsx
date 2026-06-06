17755a4ac7bf75b2abc10468e4470f135e0d824a
Rupeshgupta1 <rupesh272002@gmail.com>
2026-06-06 22:58:51 +0530

    Shift 3D model up on desktop, space for scroller

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -18,7 +18,7 @@ const Computers = ({ screenSize }) => {
       ? [0, -2.2, -2.5]
       : screenSize === "tablet"
       ? [1.5, -2.8, -3]
-      : [1, -3.25, -2.5];
+      : [1, -3.8, -2.5];
 
   return (
     <mesh>
